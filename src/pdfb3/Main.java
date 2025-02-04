85a667c7548a6ccf60ae0d0cc3760bceec944141
Anderson Pires <148875704+andershow31@users.noreply.github.com>
2025-02-04 13:49:05 -0300

    Update Main.java

--- src/pdfb3/Main.java
+++ src/pdfb3/Main.java
@@ -1,10 +1,24 @@
 package pdfb3;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        String pathFile = "src//pdfb3//arquivo.pdf"; // Caminho do arquivo
+        try (PDDocument doc = Loader.loadPDF(new File(pathFile))) { // Carrega o arquivo
+            PDFTextStripper pdfStripper = new PDFTextStripper(); // Carrega o texto do arquivo
+            String textComplet = pdfStripper.getText(doc); // Joga o texto do arquivo numa string
+            String[] linhas = textComplet.split("\\r?\\n"); // Separa a string pelas quebras de linha
+            for (String linha : linhas) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
 }
